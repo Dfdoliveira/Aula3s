f196b5ab528a71e718e361d9d677361a47c2aa87
DFDOLIVEIRA <daniela.ferreirra.oliveira@gmail.com>
2016-03-15 20:19:36 -0300

    Alteração a pedido do cliente.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -11,8 +11,8 @@ public class Principal {
 	
 	public static void main(String[] args){
 		
-		System.out.println("Olá Mundo");
-		System.out.println("É nóis!"); 
+		System.out.println("Hello World!");
+		System.out.println("It's we!"); 
 	}
 }
  

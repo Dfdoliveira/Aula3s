948edfc008e642145bbed701398e5a2aae6854b3
DFDOLIVEIRA <daniela.ferreirra.oliveira@gmail.com>
2016-03-15 20:10:36 -0300

    Usando Orinetação a Objetos...

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -1,17 +1,16 @@
 package br.univel;
 
 /**
+*
+*
 *Versão 1.0
-*
-*
 *
 */
 public class Principal {
 
 	
 	public static void main(String[] args){
-		
-		System.out.println("Olá Mundo");
+		 new Mrssage().hello();
 		System.out.println("É nóis!"); 
 	}
 }
